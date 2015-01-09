ed4086651895f22f92edf8bb6a2104c7da10adc9
Marc Alexander <admin@m-a-styles.de>
2015-01-09 17:28:46 +0100

    Add composer.json and empty library file

---
+++ lib/OTPAuthenticate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package OTPAuthenticate
+ * @copyright (c) Marc Alexander <admin@m-a-styles.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace marc1706\OTPAuthenticate;
+
+class OTPAuthenticate
+{
+
+}
